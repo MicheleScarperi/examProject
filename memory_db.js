97c4bfd81ebcb45cd183fa7d034ed92e2ceeea64
Michele <michele.scarperi@gmail.com>
2019-03-21 14:04:47 +0100

    db changes

--- memory_db.js
+++ memory_db.js
@@ -4,7 +4,71 @@ var cardsDb = [{
     urlSecondImg: "https://middleeasttransparent.com/wp-content/uploads/2017/10/TUMP.jpg",
 }, {
     name: "Merkel",
-    urlFirstImg: "https://middleeasttransparent.com/wp-content/uploads/2017/10/TUMP.jpg", 
-    urlSecondImg: "https://middleeasttransparent.com/wp-content/uploads/2017/10/TUMP.jpg",  
+    urlFirstImg: "", 
+    urlSecondImg: "",  
+}, {
+    name: "Brian",
+    urlFirstImg: "", 
+    urlSecondImg: "", 
+}, {
+    name: "Einstein",
+    urlFirstImg: "", 
+    urlSecondImg: "", 
+}, {
+    name: "Messi",
+    urlFirstImg: "", 
+    urlSecondImg: "", 
+}, {
+    name: "Fyre",
+    urlFirstImg: "", 
+    urlSecondImg: "", 
+}, {
+    name: "",
+    urlFirstImg: "", 
+    urlSecondImg: "", 
+}, {
+    name: "",
+    urlFirstImg: "", 
+    urlSecondImg: "", 
+}, {
+    name: "",
+    urlFirstImg: "", 
+    urlSecondImg: "", 
+}, {
+    name: "",
+    urlFirstImg: "", 
+    urlSecondImg: "", 
+}, {
+    name: "",
+    urlFirstImg: "", 
+    urlSecondImg: "", 
+}, {
+    name: "",
+    urlFirstImg: "", 
+    urlSecondImg: "", 
+}, {
+    name: "",
+    urlFirstImg: "", 
+    urlSecondImg: "", 
+}, {
+    name: "",
+    urlFirstImg: "", 
+    urlSecondImg: "", 
+}, {
+    name: "",
+    urlFirstImg: "", 
+    urlSecondImg: "", 
+}, {
+    name: "",
+    urlFirstImg: "", 
+    urlSecondImg: "", 
+}, {
+    name: "",
+    urlFirstImg: "", 
+    urlSecondImg: "", 
+}, {
+    name: "",
+    urlFirstImg: "", 
+    urlSecondImg: "", 
 }]
 
