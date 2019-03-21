710c8e41844bad1515f970afd302a5b876277239
andreasboers <anbo19ac@student.cbs.dk>
2019-03-21 13:58:55 +0100

    nd lirejarbkw

--- memory_db.js
+++ memory_db.js
@@ -1,6 +1,6 @@
 var cardsDb = [{
     name: "trump",
-    urlFirstImg: "https://middleeasttransparent.com/wp-content/uploads/2017/10/TUMP.jpg", 
+    urlFirstImg: "https://middleeasttransparent.com/wp-content/uploads/2017/10/TUMP.jpg",
     urlSecondImg: "https://middleeasttransparent.com/wp-content/uploads/2017/10/TUMP.jpg",
 }, {
     name: "Merkel",
