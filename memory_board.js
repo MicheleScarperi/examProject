e91cd18f67ffee47cacb27ba5a5050f12417e27f
Michele <michele.scarperi@gmail.com>
2019-03-21 13:48:50 +0100

    commit for a lot of stuff (js html)

--- memory_board.js
+++ memory_board.js
@@ -1,1 +1,15 @@
+//this will only run, when the page is loaded.
+document.addEventListener('DOMContentLoaded', initializeGame, false);
 
+function initializeGame() {
+    cardsDb.forEach(function(pairOfCards){
+        var html = `<div onclick="flipCard()" class="card" data-attribute="${pairOfCards.name}">
+                        <img src="${pairOfCards.urlFirstImg}">
+                    </div>
+                    <div onclick="flipCard()" class="card" data-attribute="${pairOfCards.name}">
+                        <img src="${pairOfCards.urlSecondImg}">
+                    </div>`;
+        var container = document.getElementById('cards-wrapper')
+        container.insertAdjacentHTML('afterbegin', html);
+    })
+}
--- memory_db.js
+++ memory_db.js
@@ -1,1 +1,10 @@
+var cardsDb = [{
+    name: "trump",
+    urlFirstImg: "https://middleeasttransparent.com/wp-content/uploads/2017/10/TUMP.jpg", 
+    urlSecondImg: "https://middleeasttransparent.com/wp-content/uploads/2017/10/TUMP.jpg",
+}, {
+    name: "Merkel",
+    urlFirstImg: "https://middleeasttransparent.com/wp-content/uploads/2017/10/TUMP.jpg", 
+    urlSecondImg: "https://middleeasttransparent.com/wp-content/uploads/2017/10/TUMP.jpg",  
+}]
 
