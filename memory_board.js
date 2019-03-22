d2d17445061131901086975afec72be5e2f9553a
Lasse Hulgaard <lassehulgaard@gmail.com>
2019-03-22 12:25:36 +0100

    Changed stuff

--- memory_board.js
+++ memory_board.js
@@ -4,12 +4,12 @@ document.addEventListener('DOMContentLoaded', initializeGame, false);
 function initializeGame() {
     cardsDb.forEach(function(pairOfCards){
         var html = `<div onclick="flipCard()" class="back-card" data-attribute="${pairOfCards.name}">
-                        <img src="${pairOfCards.urlFirstImg}" height="92" width="92" opacity: 0.5>
-                        <img src="${pairOfCards.front_face}" height="92" width="92">
+                        <img class="img1" src="${pairOfCards.urlFirstImg}" height="92" width="92" opacity: 0.5>
+                        <img class="img2" src="${pairOfCards.front_face}" height="92" width="92">
                     </div>
                     <div onclick="flipCard()" class="back-card" data-attribute="${pairOfCards.name}">
-                        <img src="${pairOfCards.urlSecondImg}" height="92" width="92" opacity: 0.5;>
-                        <img src="${pairOfCards.front_face}" height="92" width="92">
+                        <img class="img1" src="${pairOfCards.urlSecondImg}" height="92" width="92" opacity: 0.5;>
+                        <img class="img2" src="${pairOfCards.front_face}" height="92" width="92">
                     </div>`;
         var container = document.getElementById('cards-wrapper')
         container.insertAdjacentHTML('afterbegin', html); //this copies the html element and inserts it afterbegin, the copy is the html var
