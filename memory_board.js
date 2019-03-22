5decdf5ccb937780c5eed70bf4084c52ebafea00
Lasse Hulgaard <lassehulgaard@gmail.com>
2019-03-22 09:07:19 +0100

    Update memory_board.js

--- memory_board.js
+++ memory_board.js
@@ -4,12 +4,13 @@ document.addEventListener('DOMContentLoaded', initializeGame, false);
 function initializeGame() {
     cardsDb.forEach(function(pairOfCards){
         var html = `<div onclick="flipCard()" class="card" data-attribute="${pairOfCards.name}">
-                        <img src="${pairOfCards.urlFirstImg}">
+                        <img src="${pairOfCards.urlFirstImg}" height="92" width="92">
                     </div>
                     <div onclick="flipCard()" class="card" data-attribute="${pairOfCards.name}">
-                        <img src="${pairOfCards.urlSecondImg}">
+                        <img src="${pairOfCards.urlSecondImg}" height="92" width="92">
                     </div>`;
         var container = document.getElementById('cards-wrapper')
         container.insertAdjacentHTML('afterbegin', html);
     })
 }
+
