5b80f3eb2278d1a905628ceea7e1048af4d0a6dc
Michele <michele.scarperi@gmail.com>
2019-03-22 10:59:34 +0100

    changed cardsDB added new property

--- memory_db.js
+++ memory_db.js
@@ -2,73 +2,99 @@ var cardsDb = [{
     name: "trump",
     urlFirstImg: "img/trump.jpg",
     urlSecondImg: "img/maga.jpg",
+    
 }, {
     name: "Merkel",
     urlFirstImg: "img/angela.jpg", 
     urlSecondImg: "img/cdu.jpg",
+    
 }, {
     name: "Brian",
     urlFirstImg: "img/brian.jpg", 
     urlSecondImg: "img/badluck.jpg",
+    
 }, {
     name: "Einstein",
     urlFirstImg: "img/einstein.jpg", 
     urlSecondImg: "img/einsteine.png", 
+    
 }, {
     name: "Messi",
     urlFirstImg: "img/messi.jpg", 
     urlSecondImg: "img/goat.png",
+    
 }, {
     name: "Fyre",
     urlFirstImg: "img/tile.jpg", 
-    urlSecondImg: "img/tilematch.png", 
+    urlSecondImg: "img/tilematch.png",
+    
 }, {
     name: "CocoChanel",
     urlFirstImg: "img/coco_chanel.jpg", 
-    urlSecondImg: "img/text_coco_chanel.jpg", 
+    urlSecondImg: "img/text_coco_chanel.jpg",
+    
 }, {
     name: "KanyeWest",
     urlFirstImg: "img/kanye_west.jpg", 
-    urlSecondImg: "img/text_kanye_west.jpg", 
+    urlSecondImg: "img/text_kanye_west.jpg",
+     
 }, {
     name: "TonyRobbins",
     urlFirstImg: "img/tony_robbins.jpeg", 
-    urlSecondImg: "img/text_tony_robbins.jpg", 
+    urlSecondImg: "img/text_tony_robbins.jpg",
+     
 }, {
     name: "Aristotle",
     urlFirstImg: "img/aristotle.jpg", 
-    urlSecondImg: "img/text_aristotle.jpg", 
+    urlSecondImg: "img/text_aristotle.jpg",
+     
 }, {
     name: "Cesar",
     urlFirstImg: "img/julio-cesar.jpg", 
     urlSecondImg: "img/text_julius_cesar.jpg", 
+    
 }, {
     name: "TheresaMay",
     urlFirstImg: "img/theresa_may.jpg", 
     urlSecondImg: "img/text_theresa_may.jpg", 
+    
 }, {
     name: "SherlySandberg",
     urlFirstImg: "img/sherly_sandberg.jpg", 
     urlSecondImg: "img/text_sherly_sandberg.jpg", 
+    
 }, {
     name: "Chopra",
     urlFirstImg: "img/Chopra.jpg", 
     urlSecondImg: "img/text_chopra.jpg", 
+    
 }, {
     name: "Andretti",
     urlFirstImg: "img/andretti.jpg", 
-    urlSecondImg: "img/text_andretti.jpg", 
+    urlSecondImg: "img/text_andretti.jpg",
+    
 }, {
     name: "Jobs",
     urlFirstImg: "img/jobs.png", 
     urlSecondImg: "img/text_jobs.jpg", 
+    
 }, {
     name: "Obama",
     urlFirstImg: "img/obama.jpeg", 
-    urlSecondImg: "img/text_obama.png", 
+    urlSecondImg: "img/text_obama.png",
+   
 }, {
     name: "Macron",
     urlFirstImg: "img/macron.jpg", 
     urlSecondImg: "img/text_macron.png", 
+    
 }]
 
+cardsDb.forEach(function(element) {
+
+    //cardsDb.front_face = "img/cbs.png";
+    element.front_face = "img/cbs.png";
+    
+})
+
+console.log(cardsDb);
