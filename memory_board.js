f58dc09106fdab887e9f5c1968c4c10f47f6fa63
Lasse Hulgaard <lassehulgaard@gmail.com>
2019-03-22 09:24:22 +0100

    Update memory_board.js

--- memory_board.js
+++ memory_board.js
@@ -4,10 +4,10 @@ document.addEventListener('DOMContentLoaded', initializeGame, false);
 function initializeGame() {
     cardsDb.forEach(function(pairOfCards){
         var html = `<div onclick="flipCard()" class="card" data-attribute="${pairOfCards.name}">
-                        <img src="${pairOfCards.urlFirstImg}">
+                        <img src="${pairOfCards.urlFirstImg}" height="92" width="92">
                     </div>
                     <div onclick="flipCard()" class="card" data-attribute="${pairOfCards.name}">
-                        <img src="${pairOfCards.urlSecondImg}">
+                        <img src="${pairOfCards.urlSecondImg}" height="92" width="92">
                     </div>`;
         var container = document.getElementById('cards-wrapper')
         container.insertAdjacentHTML('afterbegin', html); //this copies the html element and inserts it afterbegin, the copy is the html var
