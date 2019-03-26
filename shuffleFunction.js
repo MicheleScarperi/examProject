6206f8c3bdeae7f8b8fc0a72261f63581f119210
andreasboers <anbo19ac@student.cbs.dk>
2019-03-26 09:19:09 +0100

    shuf

--- shuffleFunction.js
+++ shuffleFunction.js
@@ -19,7 +19,7 @@ function shuffle(array) {
   
   // Create a function to flip the picture on the game //
   
-  var array = [1, 1, 2, 2, 3, 3, 4, 4, 5, 5, 6, 6, 7, 7, 8, 8, 9, 9, 10, 10, 11, 11, 12, 12, 13, 13, 14, 14, 15, 15, 16, 16, 17, 17, 18, 18];
+  var array = cardsDb;
   
   // Shuffle array "elements"
   
