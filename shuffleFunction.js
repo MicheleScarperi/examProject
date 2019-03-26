3dbbf58cd6dfbbf2986756fd08dd18e6789b67fa
andreasboers <anbo19ac@student.cbs.dk>
2019-03-26 09:12:32 +0100

    Create shuffleFunction.js

---
+++ shuffleFunction.js
@@ -0,0 +1,28 @@
+function shuffle(array) {
+    var currentIndex = array.length, temporaryValue, randomIndex;
+  
+    // While there remain elements to shuffle...
+    while (0 !== currentIndex) {
+  
+      // Pick a remaining element...
+      randomIndex = Math.floor(Math.random() * currentIndex);
+      currentIndex -= 1;
+  
+      // And swap it with the current element.
+      temporaryValue = array[currentIndex];
+      array[currentIndex] = array[randomIndex];
+      array[randomIndex] = temporaryValue;
+    }
+  
+    return array;
+  }
+  
+  // Create a function to flip the picture on the game //
+  
+  var array = [1, 1, 2, 2, 3, 3, 4, 4, 5, 5, 6, 6, 7, 7, 8, 8, 9, 9, 10, 10, 11, 11, 12, 12, 13, 13, 14, 14, 15, 15, 16, 16, 17, 17, 18, 18];
+  
+  // Shuffle array "elements"
+  
+  array = shuffle(array);
+  console.log(array);
+  
