cf0dfc88ee98b52474d134da3ba3c3d602f4881d
andreasboers <anbo19ac@student.cbs.dk>
2019-03-29 08:28:27 +0100

    Marten changes

--- memory_board.js
+++ memory_board.js
@@ -2,7 +2,12 @@
 document.addEventListener('DOMContentLoaded', initializeGame, false);
 
 function initializeGame() {
+  //  var html = '' MARTEN
     cardsDb.forEach(function(cardInput){
+        
+    // html += cardInput.createHTML()
+        
+
         var html = `<div onclick="flipCard(this)" class="back_card" data-attribute="${cardInput.name}">
                         <img id="${cardInput.name}" class="img1" src="${cardInput.img}" height="110" width="110" opacity: 0.5>
                         <img class="img2" src="${cardInput.front_face}" height="110" width="110">
@@ -18,7 +23,14 @@ const cards = document.getElementsByClassName('back_card');
 // This adds an event listener to all cards in the DB. To check later on whether a card has been flipped or not. 
 
 function flipCard(element){
+
+    
+
+  //  console.log(element.dataset.attribute)
     if(counter === 2){
+      //  setTimeout(() => {
+        //    console.log('Reset cards')
+       // }, 2000)
         return;
     } 
     element.classList.toggle('flip');
