91ae0b6c82f44069cddc8f771cdddd0ffca132e4
Michele <michele.scarperi@gmail.com>
2019-03-29 11:55:20 +0100

    creation of user class and new html for player names

---
+++ namesAndStatsDB.js
@@ -0,0 +1,20 @@
+// we will use local storage to safe players names and game results here. 
+
+class User {
+    constructor(name){
+        this.name = name; 
+    }
+}
+const submitBtnUI = document.getElementById('submit-btn')
+
+submitBtnUI.onclick = function () {
+
+    console.log('clicked')
+
+    // If the user has agreed to the terms and conditions,
+    // I create a new user object from his input
+    var name = name.value 
+    // To store the user I add him to the array of users (DB)
+    //users.push(new User(firstName, lastName, username, password))
+    //redirect to game board here needed. 
+    }
