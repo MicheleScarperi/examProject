91850c81ac133ca4d31c8ad8a4ea7961908d44ff
Michele <michele.scarperi@gmail.com>
2019-06-03 10:31:06 +0200

    updated login check

--- onClickFunction.js
+++ onClickFunction.js
@@ -1,28 +1,26 @@
 //On click, save players inserted names and push them to the players array + save to localStorage.
 submitBtnUI.onclick = function () {
     var playerName = playerName1UI.value;
-
-    new Player(playerName1UI.value);
-
-
-    players.push(new Player(playerName, 0, 0));
-
-    playerName = playerName2UI.value;
-
-    new Player(playerName2UI.value);
+    var playerName2 = playerName2UI.value;
 
 
-    players.push(new Player(playerName, 0, 0));
 
-    savePlayerNames();
-    loadPlayerNames();
-
-    window.location.href = "./memory_board.html";
-    
-    if(!playerName){
-        console.log('nothing')
+    if (playerName == "" || playerName2 == "") {
         alert('Please insert names')
-        window.location.href = "./createPlayers.html";
-        return;
-    } 
+
+    } else {
+        new Player(playerName1UI.value);
+        new Player(playerName2UI.value);
+
+        players.push(new Player(playerName, 0, 0));
+        players.push(new Player(playerName2, 0, 0));
+
+
+
+
+        savePlayerNames();
+        loadPlayerNames();
+
+        window.location.href = "./memory_board.html";
+    }
 }
